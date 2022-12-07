28786e541ea2852d23b5f7e4275557cd9f273a1c
Linventif <linventif@gmail.com>
2022-12-07 22:27:22 +0100

    2022 12 07

---
+++ lua/autorun/premade.lua
@@ -0,0 +1,51 @@
+if !LinvLib then
+    print(" ")
+    print(" ")
+    print(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print(" -                                                           - ")
+    print(" -               Linventif Library is missing !              - ")
+    print(" -                                                           - ")
+    print(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print(" ")
+    print("Linventif Library is missing ! Please install it !")
+    print(" ")
+    print("You can download the latest version here : https://github.com/linventif/gmod-lib")
+    print("Or you can download it directly from the workshop : https://steamcommunity.com/sharedfiles/filedetails/?id=2882747990")
+    print("If you have any questions, you can join my discord : https://linventif.fr/discord")
+    print("If you don't download it, you won't be able to use my creations.")
+    print(" ")
+    print(" ")
+    return
+end
+
+// -- // -- // -- // -- // -- // -- // -- // -- // -- //
+
+local folder = "premade"
+local name = "PreMade"
+local full_name = "PreMade"
+local workshop = {}
+local license = "CC BY-SA 4.0"
+local version = "0.1.0"
+
+Premade = {}
+Premade.Config = {}
+LinvLib.Install["premade"] = version
+
+// -- // -- // -- // -- // -- // -- // -- // -- // -- //
+
+include(folder .. "/sh_config.lua")
+print("| " .. name .. " | File Load | " .. folder .. "/sh_config.lua")
+
+if SERVER then
+    AddCSLuaFile(folder .. "/sh_config.lua")
+    LinvLib.LoadWorkshop(workshop, name)
+    LinvLib.LoadMaterials(folder .. "/materials", name)
+end
+
+LinvLib.Loader(folder .. "/shared", name)
+LinvLib.Loader(folder .. "/server", name)
+LinvLib.Loader(folder .. "/client", name)
+LinvLib.LoadStr(full_name, version, license)
+
+print(" ")
+print(" ")
