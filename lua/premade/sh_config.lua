68a2728b34f8bd1f142d63b9a20a6e7bde24a189
Linventif <linventif@gmail.com>
2023-01-05 17:23:09 +0100

    2023 01 05

--- lua/premade/sh_config.lua
+++ lua/premade/sh_config.lua
@@ -1,1 +1,19 @@
---
+// -- // -- // -- // -- // -- // -- // -- //
+//                                        //
+//           General Settings             //
+//                                        //
+// -- // -- // -- // -- // -- // -- // -- //
+
+// General Settings
+FriendsSys.Config.Language = "french" // Language (french - english) you can add your own language in sh_language.lua
+FriendsSys.Config.Theme = "new-gen" // Theme (sentro, new-gen)
+FriendsSys.Config.Admin = { // Admins
+    ["superadmin"] = true,
+    ["admin"] = true
+}
+FriendsSys.Config.Commands = { // Commands
+    ["!friends"] = true,
+    ["!friend"] = true,
+    ["!amis"] = true,
+    ["!ami"] = true
+}
--- lua/premade/sh_lang.lua
+++ lua/premade/sh_lang.lua
@@ -1,1 +1,15 @@
---
+// If you want to add a official default language, contact me on discord: https://linventif.fr/discord
+
+local languages = {
+    ["french"] = {
+        ["close"] = "Fermer",
+        ["continue"] = "Continuer",
+        ["number_only"] = "Nombre Uniquement",
+        ["dropmoney"] = "Combien voulez-vous déposer ?"
+    }
+}
+
+// Do not edit below this line !!
+function SentroContext:GetTrad(trad)
+    return languages[SentroContext.Config.Language][trad] || languages["english"][trad] || trad
+end
